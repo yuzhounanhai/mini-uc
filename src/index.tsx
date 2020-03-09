6895bb2584540c55cdd0ee5d36dfadccda645004
yuzhounanhai <yuzhounanhai@vip.qq.com>
2020-03-09 18:28:08 +0800

    export Interceptor

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import AnimationFade from '@/components/Animation/Fade';
 import AnimationSlide from '@/components/Animation/Slide';
 import SelectMenu from '@/components/SelectMenu';
+import Interceptor from '@/components/Interceptor';
+
 export {
   AnimationFade,
   AnimationSlide,
-  SelectMenu
+  SelectMenu,
+  Interceptor,
 }
